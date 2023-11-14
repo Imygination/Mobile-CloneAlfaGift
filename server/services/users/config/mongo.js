16912e7112fe52f0af49ec93ceb34a39881024d0
Gema <gemaramadhan20@gmail.com>
2023-11-14 15:38:34 +0700

    Final Deploy V3

--- server/services/users/config/mongo.js
+++ server/services/users/config/mongo.js
@@ -1,5 +1,5 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri = `mongodb://gemaramadhan20:${process.env.MONGO_PASS}@ac-3vkg2mn-shard-00-00.zjteeqn.mongodb.net:27017/?ssl=true&replicaSet=atlas-93jrv1-shard-0&authSource=admin&retryWrites=true&w=majority`;
+const uri = `mongodb://gemaramadhan20:ksamXrZXK2FAFyAo@ac-3vkg2mn-shard-00-00.zjteeqn.mongodb.net:27017,ac-3vkg2mn-shard-00-01.zjteeqn.mongodb.net:27017,ac-3vkg2mn-shard-00-02.zjteeqn.mongodb.net:27017/?ssl=true&replicaSet=atlas-93jrv1-shard-0&authSource=admin&retryWrites=true&w=majority`;
 const client = new MongoClient(uri);
 
 let db = null;
